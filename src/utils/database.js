7602727b9854a14efe691be923a9e9bb4971f0ce
Lapecru64 <perlura64@outlook.com>
2023-09-12 00:47:15 -0600

    Entregable-3

--- src/utils/database.js
+++ src/utils/database.js
@@ -5,7 +5,7 @@ const db = new Sequelize({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  username: process.env.DB_USER,
+  username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   dialect: "postgres",
   ...(process.env.NODE_ENV === "production"
